bcb13877794061dcc36d2726026242b50a5a26b0
Nicolas <captaincallof@hotmail.com>
2020-04-10 09:35:24 +0200

    projet

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('templates.index')
+
+@section('head')
+    @include('templates.head')
+@endsection
+
+@section('body')
+    @include('templates.body')
+@endsection
+
+@section('footer')
+    @include('templates.footer')
+@endsection 
+
+@section('index')
+    @include('templates.index')
+@endsection
